5f1d609fd3eb5cfe2c301719ecb7790064f7cd13
rtbf <popovrromma@gmail.com>
2024-04-28 22:59:39 +1000

    sevenTwo

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,10 +1,11 @@
 import os 
 from flask import Flask
+from dotenv import load_dotenv, dotenv_values 
 from flask_mail import Mail # для отправки на почту
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-
+load_dotenv()
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
@@ -17,7 +18,7 @@ login_manager.login_message_category = 'info'
 app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'popovrromma@gmail.com'
-app.config['MAIL_PASSWORD'] = 'gbtm jhzr uhfg hmkf'
+app.config['MAIL_USERNAME'] = os.getenv("NAME")
+app.config['MAIL_PASSWORD'] = os.getenv("PASSWORD")
 mail = Mail(app)
 from flaskblog import routs
